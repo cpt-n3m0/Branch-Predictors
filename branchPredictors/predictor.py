cb5cdbe4a32a3a9e165b3b0c74a7b7a551f95cba
cpt-n3m0 <faroukjouti@gmail.com>
2019-10-23 19:27:40 +0100

    fixed indexing error, added gShare implementation

--- branchPredictors/predictor.py
+++ branchPredictors/predictor.py
@@ -4,6 +4,7 @@ import sys
 
 
 TABLE_SIZE=4096
+GHR_SIZE=3
 
 def alwaysTaken(trace):
     predictedTrace = []
@@ -11,9 +12,9 @@ def alwaysTaken(trace):
     
     for line in trace:
         split = line.split(" ")
-        gt = split[1][:-1] 
-        print(split[0] + " 0")
-        if gt != "0":
+        gt = split[1][0] 
+       # print(split[0] + " 1")
+        if gt != "1":
             stats["error"] += 1
         stats["numberOfBranches"] += 1
     return stats
@@ -30,14 +31,19 @@ def updateBranchState(branchState, taken):
     branchState = 3 if (branchState > 3)  else  branchState
     return branchState
 
+def get2bitIndex(iAddress, tableSize):
+    iAddressBits = "{0:b}".format(int(iAddress))
+    leastSign = iAddressBits[-8:]
+    return int(leastSign, 2) % TABLE_SIZE
+
 def twoBit(trace):
     table = {}
     stats = {"error": 0, "numberOfBranches": 0}
 
     for line in trace:
         split = line.split(" ")
-        index = str(int(split[0][7:]) % TABLE_SIZE)
-        gt = split[1]
+        index = get2bitIndex(split[0], TABLE_SIZE)
+        gt = split[1][0]
         predict = None
         try :
             if table[index] < 2:
@@ -53,12 +59,46 @@ def twoBit(trace):
             stats["error"]  += 1
         table[index] = updateBranchState(table[index], bool(int(gt)))
 
-       # if table[index] >= 2 and predictTaken:
-       #     stats["error"] += 1
-       # elif table[index] < 2 and not predictTaken:
-       #     stats["error"] += 1
         stats["numberOfBranches"] += 1
 
+    return stats
+
+
+def getGShareIndex(iAddress, pattern):
+    iAddressBits = "{0:b}".format(int(iAddress))
+    leastSign = iAddressBits[-8:]
+
+    return int(leastSign, 2) ^  int(pattern, 2)
+
+def shiftRegisterLeft(register, newEntry):
+    register = register[1:]
+    register += newEntry
+    return register
+
+def gShare(trace):
+    globalHistoryRegister = "0" * GHR_SIZE
+    predictionTable = {}
+    stats = {"error": 0, "numberOfBranches": 0}
+
+    for line  in trace:
+        split = line.split(" ")
+        index = getGShareIndex(int(split[0]), (globalHistoryRegister))
+        gt = split[1][0]
+
+        try :
+            if predictionTable[index] < 2:
+                predictTaken = 0
+            else:
+                predictTaken = 1
+        except(KeyError):
+            predictionTable[index] = 0
+            predictTaken = 0
+        if predictTaken != int(gt) :
+            stats["error"]  += 1
+
+        predictionTable[index] = updateBranchState(predictionTable[index], bool(int(gt)))
+        globalHistoryRegister = shiftRegisterLeft(globalHistoryRegister, gt)
+        stats["numberOfBranches"] += 1
     return stats
 
 
@@ -69,6 +109,8 @@ def main(tracePath, predictor):
             stats = alwaysTaken(trace)
         if(predictor == "2bit"):
             stats = twoBit(trace)
+        if(predictor == "gShare"):
+            stats = gShare(trace)
     print("Number of branches: " + str(stats["numberOfBranches"]))
     print("Success rate : "  +  str(100 - (stats["error"] * 100)/stats["numberOfBranches"]))
 
