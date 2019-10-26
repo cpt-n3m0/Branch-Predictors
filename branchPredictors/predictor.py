65b9c61571415a1bacc012d989bc95066374c4a0
fj20 <fj20@pc7-146-l.cs.st-andrews.ac.uk>
2019-10-26 18:31:22 +0100

    implemented low level profiled approach

--- branchPredictors/predictor.py
+++ branchPredictors/predictor.py
@@ -9,6 +9,15 @@ TABLE_SIZE= 4096
 GHR_SIZE=4
 ADDRESS_I_SIZE = 8
 
+#Profiled config
+HIGHLEVEL = True
+TCOUNT_SIZE = 16
+OCOUNT_SIZE = 16
+BRANCHMAP_SIZE = PIS = 24
+
+PROFILE_TABLE_SIZE = 24
+
+
 def alwaysTaken(trace):
     predictedTrace = []
     stats = {"error": 0, "numberOfBranches": 0}
@@ -34,11 +43,11 @@ def updateBranchState(branchState, taken):
     branchState = 3 if (branchState > 3)  else  branchState
     return branchState
 
-def get2bitIndex(iAddress, tableSize):
+def getLeastIndex(iAddress, tableSize, bitSize):
     iAddressBits = "{0:b}".format(int(iAddress))
-    leastSign = iAddressBits[-ADDRESS_I_SIZE:]
+    leastSign = iAddressBits[-bitSize:]
 #    print(leastSign)
-    return hash(str(int(leastSign, 2))) % tableSize 
+    return int(leastSign, 2) % tableSize 
 
 def twoBit(trace):
     predictionTable = {}
@@ -46,7 +55,7 @@ def twoBit(trace):
 
     for line in trace:
         split = line.split(" ")
-        index = get2bitIndex(split[0], TABLE_SIZE)
+        index = getLeastIndex(split[0], TABLE_SIZE, ADDRESS_I_SIZE)
 #        print(index)
         gt = split[1][0]
         predict = None
@@ -94,7 +103,6 @@ def gShare(trace):
     for line  in trace:
         split = line.split(" ")
         index = getGShareIndex(split[0], (globalHistoryRegister), TABLE_SIZE)
-        print(index)
         gt = split[1][0]
 
         if index in predictionTable :
@@ -115,7 +123,41 @@ def gShare(trace):
 
 
 
-def createProfile(trace, out):
+def createProfileLowLevel(trace, out):
+    branchStatusTable = {}
+    profile = {}
+    counter = 0
+    for line in trace:
+        split = line.split(" ")
+        iAddress = split[0]
+        gt = split[1][0]
+        index = getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE), PIS)
+        if index in branchStatusTable:
+            tCount = branchStatusTable[index]["taken_count"] 
+            oCount = branchStatusTable[index]["occurence"] 
+            branchStatusTable[index]["taken_count"] = tCount + int(gt) if tCount < pow(2, TCOUNT_SIZE) else tCount
+            branchStatusTable[index]["occurence"] = oCount + 1 if oCount < pow(2, OCOUNT_SIZE) else oCount
+            
+        elif counter < pow(2, PROFILE_TABLE_SIZE):
+            counter += 1
+            branchStatusTable[index] = {"occurence" : 1, "taken_count" : int(gt), "order" : counter}
+        else:
+            print("profile table saturated.")
+            
+
+    for branch in branchStatusTable :
+        b = branchStatusTable[branch]
+        profile[b["order"]] = int(((b["taken_count"] * 100)/b["occurence"]) > 50 )
+
+    if not os.path.exists("profiles"):
+        os.makedirs("profiles")
+    with open("profiles/" +out, "w") as outFile:
+        outFile.write(json.dumps( profile, indent=4, sort_keys=True))
+    with open("dump.out", "w") as dump :
+        dump.write(json.dumps(branchStatusTable, indent=4, sort_keys=True))
+
+
+def createProfileHighLevel(trace, out):
     branchStatusTable = {}
     profile = {}
     counter = 0
@@ -134,8 +176,10 @@ def createProfile(trace, out):
         b = branchStatusTable[branch]
         profile[b["order"]] = int(((b["taken_count"] * 100)/b["occurence"]) > 50 )
 
-    with open(out, "w") as outFile:
-        outFile.write(json.dumps(profile, indent=4, sort_keys=True))
+    if not os.path.exists("profiles"):
+        os.makedirs("profiles")
+    with open("profiles/" +out, "w") as outFile:
+        outFile.write(json.dumps( profile, indent=4, sort_keys=True))
     with open("dump.out", "w") as dump :
         dump.write(json.dumps(branchStatusTable, indent=4, sort_keys=True))
 
@@ -146,15 +190,16 @@ def profiledPredict(trace, proFile):
     counter = 0
     for line in trace:
         split = line.split(" ")
-        address = split[0]
+        iAddress = split[0]
         gt = split[1]
-        if address not in branchMap:
+        index = iAddress if HIGHLEVEL else getLeastIndex(iAddress, pow(2, BRANCHMAP_SIZE),  PIS) 
+        if index not in branchMap:
             counter += 1
-            branchMap[address] = counter
-        if str(branchMap[address]) in profile:
-            prediction = profile[str(branchMap[address])] 
+            branchMap[index] = counter
+        if str(branchMap[index]) in profile:
+            prediction = profile[str(branchMap[index])] 
         else:
-            prediction = 0
+            prediction = 1
         if prediction != int(gt):
             stats["error"] += 1
 
@@ -179,7 +224,10 @@ def main(tracePath, predictor, profile=None):
         if(predictor == "gShare"):
             stats = gShare(trace)
         if(predictor == "createProfile"):
-            createProfile(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
+            if HIGHLEVEL:
+                createProfileHighLevel(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
+            else:
+                createProfileLowLevel(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
         if(predictor == "profiled"):
             with open(profile, "r") as pf:
                 stats = profiledPredict(trace, pf)
@@ -192,9 +240,19 @@ try :
     tracePath = sys.argv[1]
     command = sys.argv[2]
     if command == "profiled" :
-        profile = sys.argv[3]
+        profile = sys.argv[3]        
     else:
         profile = None
+
+    if command in ["profiled", "createProfile"]:
+        lvl = sys.argv[-1]
+        if lvl == "--high":
+            HIGHLEVEL = True
+        elif lvl =="--low":
+            HIGHLEVEL = False
+        else:
+            print("ERROR: missing level flag")
+            sys.exit(-1)
 except:
     print("ERROR: insufficient number of arguments")
     sys.exit(-1)
