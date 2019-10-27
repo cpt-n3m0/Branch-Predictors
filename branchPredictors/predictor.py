b1efa053a8c46c57215fe15d352f266a1ba9c7a1
fj20 <fj20@pc7-115-l.cs.st-andrews.ac.uk>
2019-10-27 13:54:29 +0000

    completed profiled

--- branchPredictors/predictor.py
+++ branchPredictors/predictor.py
@@ -10,12 +10,13 @@ GHR_SIZE=4
 ADDRESS_I_SIZE = 8
 
 #Profiled config
-HIGHLEVEL = True
+HIGHLEVEL = False
+SINGLESTRUCTURE = False
+
 TCOUNT_SIZE = 16
 OCOUNT_SIZE = 16
-BRANCHMAP_SIZE = PIS = 24
-
-PROFILE_TABLE_SIZE = 24
+PROFILE_TABLE_SIZE = 16
+PIS = 16
 
 
 def alwaysTaken(trace):
@@ -88,7 +89,7 @@ def getGShareIndex(iAddress, pattern, tableSize):
 #    print("pattern : " + pattern)
 
 #    print("result  : " +("{0:b}".format(int(leastSign, 2) ^  int(pattern, 2))))
-    return hash(str(int(leastSign, 2) ^  int(pattern, 2))) % tableSize
+    return int(leastSign, 2) ^  int(pattern, 2) % tableSize
 
 def shiftRegisterLeft(register, newEntry):
     register = register[1:]
@@ -122,6 +123,45 @@ def gShare(trace):
     return stats
 
 
+def createProfileLowLevelSingleStructure(trace, out):
+    profile = {}
+    counter = 0
+    lc = 0
+    for line in trace:
+        split = line.split(" ")
+        iAddress = split[0]
+        gt = split[1][0]
+        exists = False
+        for index in profile:
+            entry = profile[index]
+            if entry["tag"] == getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE), PIS):
+                tCount = entry["taken_count"] 
+                oCount = entry["occurence"] 
+                entry["taken_count"] = tCount + int(gt) if tCount < pow(2, TCOUNT_SIZE) else tCount
+                entry["occurence"] = oCount + 1 if oCount < pow(2, OCOUNT_SIZE) else oCount
+                exists = True
+                
+        if not exists and counter < pow(2, PROFILE_TABLE_SIZE) :
+            counter += 1
+            profile[counter] = {"occurence" : 1, "taken_count" : int(gt), "tag" : getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE), PIS), "adviceBit": 0}
+            if not counter % 100:
+                print(counter)
+        lc += 1
+        if not lc % 1000:
+            print("branch number: " + str(lc))
+    for i in profile :
+        entry = profile[i]
+        entry["adviceBit"] = int(((entry["taken_count"] * 100)/entry["occurence"]) > 50 )
+
+    if not os.path.exists("profiles"):
+        os.makedirs("profiles")
+    with open("profiles/ss_" + out, "w") as outFile:
+        outFile.write(json.dumps( profile, indent=4, sort_keys=True))
+#    with open("dump.out", "w") as dump :
+#        dump.write(json.dumps(branchStatusTable, indent=4, sort_keys=True))
+
+
+   
 
 def createProfileLowLevel(trace, out):
     branchStatusTable = {}
@@ -147,7 +187,7 @@ def createProfileLowLevel(trace, out):
 
     for branch in branchStatusTable :
         b = branchStatusTable[branch]
-        profile[b["order"]] = int(((b["taken_count"] * 100)/b["occurence"]) > 50 )
+        profile[b["order"]] =  int(((b["taken_count"] * 100)/b["occurence"]) > 50)
 
     if not os.path.exists("profiles"):
         os.makedirs("profiles")
@@ -174,14 +214,43 @@ def createProfileHighLevel(trace, out):
 
     for branch in branchStatusTable :
         b = branchStatusTable[branch]
-        profile[b["order"]] = int(((b["taken_count"] * 100)/b["occurence"]) > 50 )
+        profile[b["order"]] =  int(((b["taken_count"] * 100)/b["occurence"]) > 50 )
 
     if not os.path.exists("profiles"):
         os.makedirs("profiles")
     with open("profiles/" +out, "w") as outFile:
         outFile.write(json.dumps( profile, indent=4, sort_keys=True))
-    with open("dump.out", "w") as dump :
-        dump.write(json.dumps(branchStatusTable, indent=4, sort_keys=True))
+    #with open("dump.out", "w") as dump :
+        #dump.write(json.dumps(branchStatusTable, indent=4, sort_keys=True))
+
+
+def profiledPredictSingleStructure(trace, proFile):
+    profile = json.load(proFile)
+    stats = {"error": 0, "numberOfBranches": 0}
+    counter = 0
+
+    for line in trace:
+        split = line.split(" ")
+        iAddress = split[0]
+        gt = split[1]
+
+        exists = False
+        for i in profile:
+            if profile[i]["tag"] == getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE),  PIS):
+                prediction = int(profile[i]["adviceBit"])
+
+        if not exists:
+            prediction = 1
+
+        #index = iAddress if HIGHLEVEL else getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE),  PIS) 
+        if prediction != int(gt):
+            stats["error"] += 1
+
+        
+        stats["numberOfBranches"] += 1
+    
+    return stats
+
 
 def profiledPredict(trace, proFile):
     profile = json.load(proFile)
@@ -192,12 +261,12 @@ def profiledPredict(trace, proFile):
         split = line.split(" ")
         iAddress = split[0]
         gt = split[1]
-        index = iAddress if HIGHLEVEL else getLeastIndex(iAddress, pow(2, BRANCHMAP_SIZE),  PIS) 
+        index = iAddress if HIGHLEVEL else getLeastIndex(iAddress, pow(2, PROFILE_TABLE_SIZE),  PIS) 
         if index not in branchMap:
             counter += 1
             branchMap[index] = counter
         if str(branchMap[index]) in profile:
-            prediction = profile[str(branchMap[index])] 
+            prediction = int(profile[str(branchMap[index])])
         else:
             prediction = 1
         if prediction != int(gt):
@@ -225,34 +294,51 @@ def main(tracePath, predictor, profile=None):
             stats = gShare(trace)
         if(predictor == "createProfile"):
             if HIGHLEVEL:
+                print("creating high level profile...")                 
                 createProfileHighLevel(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
             else:
-                createProfileLowLevel(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
+                if SINGLESTRUCTURE:
+                    print("creating single structure profile...")                 
+                    createProfileLowLevelSingleStructure(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
+                else:
+                    print("creating profile...")                 
+                    createProfileLowLevel(trace, os.path.basename(tracePath).split(".")[0] + ".profile")
         if(predictor == "profiled"):
             with open(profile, "r") as pf:
-                stats = profiledPredict(trace, pf)
+                if os.path.basename(profile)[:2] == "ss":
+                    print("Single structure profiled")
+                    stats = profiledPredictSingleStructure(trace, pf)
+                else:
+                    print("Profiled")
+                    stats = profiledPredict(trace, pf)
     if len(stats):
         print("Number of branches: " + str(stats["numberOfBranches"]))
         print("Success rate : "  +  str(100 - (stats["error"] * 100)/stats["numberOfBranches"]))
 
 
+
+def is_flag(arg):
+    return arg[0] == "-"
+
 try :
     tracePath = sys.argv[1]
     command = sys.argv[2]
+
     if command == "profiled" :
         profile = sys.argv[3]        
     else:
         profile = None
+    
+    
+    for arg in sys.argv[2:]:
+        if is_flag(arg):
+            if arg.lower() == "--high":
+                HIGHLEVEL = True
+            elif arg.lower() == "--low":
+                HIGHLEVEL = False
+            elif arg.lower() == "--singlestructure":
+                SINGLESTRUCTURE = True
 
-    if command in ["profiled", "createProfile"]:
-        lvl = sys.argv[-1]
-        if lvl == "--high":
-            HIGHLEVEL = True
-        elif lvl =="--low":
-            HIGHLEVEL = False
-        else:
-            print("ERROR: missing level flag")
-            sys.exit(-1)
 except:
     print("ERROR: insufficient number of arguments")
     sys.exit(-1)
